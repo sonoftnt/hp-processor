c6f0c26d365bbcce75e1c2c8b2fd8ef74354c2e5
topnotchtrash <shivasaireddykodandapuram@gmail.com>
2023-10-13 10:30:23 +0530

    Initial Gradle Setup

---
+++ src/main/java/com/tnt/hp/HpApplication.java
@@ -0,0 +1,13 @@
+package com.tnt.hp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.*;
+
+@SpringBootApplication
+public class HpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HpApplication.class, args);
+	}
+}
